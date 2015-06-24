3e99b2d39e13794f41d15aa97fca2dd33a88b384
Josh Lee <Lee45276@gmail.com>
2015-06-24 20:58:23 +0800

    Added code to generate deck and deal cards for first turn. Renamed form route to new_player. changed login form to post to new_player page, it value already exsits redirect to game

--- main.rb
+++ main.rb
@@ -4,38 +4,39 @@ require "pry"
 
 set :sessions, true
 
-post '/' do
-  if !session[:username]
-    session[:username] = params[:username]
-  elsif session[:username] != params[:username] && (session[:username] != "" && session[:username])
-    session[:username] = params[:username]
-    redirect '/game'
-  elsif session[:username] != "" && session[:username]
-    redirect '/game'
-  else 
-    redirect '/login'
-  end
-end
 
 get '/' do
-  if !session[:username]
-    session[:username] = params[:username]
-  elsif session[:username] != "" && session[:username]
+  if session[:username]
     redirect '/game'
   else 
-    redirect 'login'
+    redirect '/new_player'
   end
 end
 
-get '/login' do
-  erb :login
+get '/new_player' do
+  erb :new_player
 end
 
+post '/new_player' do
+  session[:player_name] = params[:player_name]
+  redirect '/game'
 
+end
 
 get '/game' do
-  if !session[:username] || session[:username] == ""
-    redirect '/login'
-  end
+  # set up initial game values
+  suits = %w[H D C S]
+  values = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+  session[:deck] = suits.product(values).shuffle!
+  # deal cards
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+
+   # dealer cards
+   # player cards
   erb :game
 end
