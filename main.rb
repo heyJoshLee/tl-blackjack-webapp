5c252430cbdcb89b9d050b2809e00a048698e514
Josh Lee <Lee45276@gmail.com>
2015-06-24 16:55:21 +0800

    Set up form route to check for username.

--- main.rb
+++ main.rb
@@ -6,42 +6,33 @@ set :sessions, true
 
 
 
+post '/' do
+  session[:username] = params[:username]
+  if session[:username] != "" && session[:username]
+    redirect '/home'
+  else 
+    redirect 'form'
+  end
+end
+
 get '/' do
-  "Hello World"
+  session[:username] = params[:username]
+  if !session[:username] || session[:username] != ""
+    redirect '/form'
+  else
+    redirect '/home'
+  end
 end
 
 get '/form' do
   erb :form
 end
 
-post '/login' do
-  session[:username] = params[:username]
-  puts session[:username]
-  puts "I guess you can put middleware here"
-  redirect '/login'
-end
-
-get '/users' do
-  erb :"/users/profile"
-end
-
-get '/login' do
-  erb :login
-end
-
-get "/profile" do
-  erb :"/users/profile", layout: :"/users/user_layout"
-end
-
-get '/about_us' do
-  redirect '/about'
-end
-
-get '/about' do
-  erb :about
-end
 
 
-get '/test' do
-  erb :test
+get '/home' do
+  if !session[:username] || session[:username] == ""
+    redirect '/form'
+  end
+  erb :home
 end
