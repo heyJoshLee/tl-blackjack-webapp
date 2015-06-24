460f64a7a570bfcc00e63ddd4d48c122adcc47d7
Josh Lee <Lee45276@gmail.com>
2015-06-24 15:05:53 +0800

    Created nested templetes

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require "pry"
 
 set :sessions, true
 
@@ -7,6 +8,33 @@ set :sessions, true
 
 get '/' do
   "Hello World"
+end
+
+get '/form' do
+  erb :form
+end
+
+post '/login' do
+  session[:username] = params[:username]
+  puts session[:username]
+  puts "I guess you can put middleware here"
+  redirect '/login'
+end
+
+get '/users' do
+  erb :"/users/profile"
+end
+
+get '/login' do
+  erb :login
+end
+
+get "/profile" do
+  erb :"/users/profile", layout: :"/users/user_layout"
+end
+
+get '/about_us' do
+  redirect '/about'
 end
 
 get '/about' do
