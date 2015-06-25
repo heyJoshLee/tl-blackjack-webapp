d933811dae486e3768d8928b1463d1376583d850
Josh Lee <Lee45276@gmail.com>
2015-06-25 10:30:52 +0800

    Added ablitiy to hit and stay. Added before method which checks to see if player has busted or stayed then will display hit and stay buttons is player can keep going

--- main.rb
+++ main.rb
@@ -26,6 +26,13 @@ helpers do
   end
 end
 
+before do
+  @show_hit_or_stay_buttons = true
+end
+
+
+
+
 get '/' do
   if session[:username]
     redirect '/game'
@@ -61,3 +68,24 @@ get '/game' do
    # player cards
   erb :game
 end
+
+post '/game/player/hit' do
+  session[:player_cards] << session[:deck].pop
+  if calculate_total(session[:player_cards]) > 21
+    @error = "Sorry, you busted."
+    @show_hit_or_stay_buttons = false
+  end
+
+  erb :game
+end
+
+
+post '/game/player/stay' do
+  @success = "You have chosen to stay"
+  @show_hit_or_stay_buttons = false
+  erb :game
+  end
+
+
+
+
