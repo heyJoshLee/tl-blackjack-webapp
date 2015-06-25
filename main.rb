96caac855141963f849ebae957dc28125dcc77b2
Josh Lee <Lee45276@gmail.com>
2015-06-25 22:47:00 +0800

    Added showing cards feature and check player for blackjack

--- main.rb
+++ main.rb
@@ -24,7 +24,33 @@ helpers do
 
   total
   end
+
+  def card_image(card) # ["H", "4"]
+    suit = case card[0]
+      when "H" then "hearts"
+      when "D" then "diamonds"
+      when "C" then "clubs"
+      when "S" then "spades"
+    end
+
+    value = card[1]
+    if ['J', "Q", "K", "A"].include?(value)
+      value = case card[1]
+        when "J" then "jack"
+        when "Q" then "queen"
+        when "K" then "king"
+        when "A" then "ace"
+      end
+    end
+    
+    "<img src='/images/cards/#{suit}_#{value}.jpg' class='card_image'>"
+  end
+
+
 end
+
+
+
 
 before do
   @show_hit_or_stay_buttons = true
@@ -46,6 +72,11 @@ get '/new_player' do
 end
 
 post '/new_player' do
+  if params[:player_name].empty?
+    @error = "Name is required"
+    halt erb(:new_player)
+  end
+
   session[:player_name] = params[:player_name]
   redirect '/game'
 
@@ -71,8 +102,12 @@ end
 
 post '/game/player/hit' do
   session[:player_cards] << session[:deck].pop
-  if calculate_total(session[:player_cards]) > 21
-    @error = "Sorry, you busted."
+  player_total = calculate_total(session[:player_cards])
+  if player_total == 21
+    @success = "Congrats! #{session[:player_name]} hit blackjack"
+    @show_hit_or_stay_buttons = false
+  elsif calculate_total(session[:player_cards]) > 21
+    @error = "Sorry, #{session[:player_name]} busted."
     @show_hit_or_stay_buttons = false
   end
 
@@ -81,7 +116,7 @@ end
 
 
 post '/game/player/stay' do
-  @success = "You have chosen to stay"
+  @success = "#{session[:player_name]} has chosen to stay"
   @show_hit_or_stay_buttons = false
   erb :game
   end
