063fdece233f8f26d3051f01683c6cb6e9fa5fa6
Josh Lee <Lee45276@gmail.com>
2015-06-28 07:33:04 +0800

    Fixed jquery / chrome bug

--- main.rb
+++ main.rb
@@ -2,7 +2,9 @@ require 'rubygems'
 require 'sinatra'
 require "pry"
 
-set :sessions, true
+use Rack::Session::Cookie, :key => 'rack.session',
+                           :path => '/',
+                           :secret => 'your_secret'
 
 BLACKJACK_AMOUNT = 21
 DEALER_MIN_HIT = 17
