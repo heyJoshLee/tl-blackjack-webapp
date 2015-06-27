e690113f088ae34e92e179eae7768a83713f81f7
Josh Lee <Lee45276@gmail.com>
2015-06-27 20:57:23 +0800

    Added betting

--- main.rb
+++ main.rb
@@ -6,6 +6,7 @@ set :sessions, true
 
 BLACKJACK_AMOUNT = 21
 DEALER_MIN_HIT = 17
+INITIAL_POT_AMOUNT = 500
 
 helpers do
   def calculate_total(cards) # cards is a nested array
@@ -52,12 +53,14 @@ helpers do
   def winner!(msg)
     @play_again = true
     @show_hit_or_stay_buttons = false
+    session[:player_pot] = session[:player_pot] + session[:player_bet]
     @success = "<strong>#{session[:player_name]} wins! </strong> #{msg}"
   end
 
   def loser!(msg)
     @play_again = true
     @show_hit_or_stay_buttons = false
+    session[:player_pot] = session[:player_pot] - session[:player_bet]
     @error = "<strong> #{session[:player_name]} loses. </strong> #{msg}"
   end
 
@@ -69,9 +72,6 @@ helpers do
 
 
 end # End helpers
-
-
-
 
 before do
   @show_hit_or_stay_buttons = true
@@ -89,8 +89,17 @@ get '/' do
 end
 
 get '/new_player' do
+  session[:player_pot] = INITIAL_POT_AMOUNT
   erb :new_player
 end
+
+get '/bet' do
+  session[:player_bet] = nil
+  erb :bet
+end
+
+
+
 
 post '/new_player' do
   if params[:player_name].empty?
@@ -99,8 +108,20 @@ post '/new_player' do
   end
 
   session[:player_name] = params[:player_name]
-  redirect '/game'
+  redirect '/bet'
+end
 
+post '/bet' do
+  if params[:bet_amount].nil? || params[:bet_amount].to_i == 0
+    @error = "Must make a bet"
+    halt erb(:bet)
+  elsif params[:bet_amount].to_i > session[:player_pot]
+    @error = "Bet amount cannot be greater than what you have ($#{session[:player_pot]}"
+    halt erb(:bet)
+  else
+    session[:player_bet] = params[:bet_amount].to_i
+    redirect "/game"
+  end
 end
 
 get '/game' do
